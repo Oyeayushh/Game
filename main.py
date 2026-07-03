196e8fd636c1f99067e0940b9c4a2aea14e477ff
𝐀ʏᴜsʜ 𝐊ᴜᴍᴀʀ 𝐘ᴀᴅᴀᴠ 💞 <pragati.prajapati2009@gmail.com>
2026-07-03 13:54:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ async def main():
     import plugins.rpg          # noqa: F401
     import plugins.economy      # noqa: F401
     import plugins.group_mgmt   # noqa: F401
+    import plugins.chatbot      # noqa: F401
     print("✅ ᴘʟᴜɢɪɴs ʟᴏᴀᴅᴇᴅ.")
 
     os.makedirs("sessions", exist_ok=True)
